db08c7709f8061217957b7d755ab27b9c3109d5d
enakinena <jenyakuraev3@gmail.com>
2023-06-26 00:03:11 +0300

    Task-13 calc/edit code

--- enakinena/calc.js
+++ enakinena/calc.js
@@ -1,50 +1,58 @@
 // Calculate - 1
 
-function calc(a, b, operation) {
-  if (operation === "add") {
-    return Number(a) + Number(b);
-  } else if (operation === "subtract") {
-    return Number(a) - Number(b);
-  } else if (operation === "multi") {
-    return Number(a) * Number(b);
-  }
-}
-console.log(calc(2, 3, "add"));
+// function calc(a, b, operation) {
+//   if (operation === "add") {
+//     return Number(a) + Number(b);
+//   } else if (operation === "subtract") {
+//     return Number(a) - Number(b);
+//   } else if (operation === "multi") {
+//     return Number(a) * Number(b);
+//   }
+// }
+// console.log(calc(2, 3, "add"));
 
 // Calculate - 2
 
-function calc(a, b, operation) {
-  if (operation === "add") {
-    return a + b;
-  } else if (operation === "subtract") {
-    return a - b;
-  } else if (operation === "multi") {
-    return a * b;
-  }
-}
-console.log(calc(2, 3, "subtract"));
+// function calc(a, b, operation) {
+//   if (operation === "add") {
+//     return a + b;
+//   } else if (operation === "subtract") {
+//     return a - b;
+//   } else if (operation === "multi") {
+//     return a * b;
+//   }
+// }
+// console.log(calc(2, 3, "subtract"));
 
 // Calculate - 3 - switch case
 
+const add = "add";
+const subtract = "subtract";
+const multi = "multi";
+
 function calc(a, b, operation) {
   switch (operation) {
-    case "add":
-      alert(Number(a) + Number(b));
+    case add:
+      return Number(a) + Number(b);
       break;
 
-    case "subtract":
-      alert(a - b);
+    case subtract:
+      return a - b;
       break;
 
-    case "multi":
-      alert(a * b);
+    case multi:
+      return a * b;
       break;
 
     default:
-      alert("Что то не известное");
+      return "Что то не известное";
       break;
   }
 }
-calc(10, 2, "multi");
-calc(10, 2, "subtract");
-calc(5, 10, "add");
+// calc(4, 10, "add");
+// calc(10, 2, "subtract");
+// calc(10, 2, "multi");
+
+console.log(calc(4, 10, "add"));
+console.log(calc(10, 2, "subtract"));
+console.log(calc(10, 2, "multi"));
