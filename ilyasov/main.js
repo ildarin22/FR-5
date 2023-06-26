2b3ef4b03f22bed68cfcba75297eb3fcfb6fefae
Ildar.Ilyasov <Ildar.Ilyasov@tel-int.ru>
2023-06-26 13:28:24 +0300

    code_review

---
+++ ilyasov/main.js
@@ -0,0 +1,17 @@
+﻿function calc_1(op, a, b) {
+    switch(op) {
+        case 'add':
+            return  a+b;
+        case 'multi':
+            return a*b;
+        case 'sub':
+            return a-b;
+    }
+    
+}
+
+function calc_2(op, a, b) {
+    if (op=='add') return a+b;
+    else if (op=='multu') return a*b;
+    else if (op=='sub') return a-b;
+}
